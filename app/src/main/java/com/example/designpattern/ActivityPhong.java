89ec922e267aefbb5bcfdf65bfe6716c46fb3ab3
vanphong19 <phongdv1901@gmail.com>
2024-06-01 22:34:38 +0700

    add dtb

--- app/src/main/java/com/example/designpattern/ActivityPhong.java
+++ app/src/main/java/com/example/designpattern/ActivityPhong.java
@@ -63,6 +63,7 @@ public class ActivityPhong extends BaseActivity {
         if(actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
     }
 
     private List<HomePage> getListContent() {
