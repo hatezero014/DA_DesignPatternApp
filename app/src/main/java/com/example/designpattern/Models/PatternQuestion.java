25972b27a8b9ff769236c7ff13728511fd5909e9
vanphong19 <phongdv1901@gmail.com>
2024-06-24 21:50:57 +0700

    change language

--- app/src/main/java/com/example/designpattern/Models/PatternQuestion.java
+++ app/src/main/java/com/example/designpattern/Models/PatternQuestion.java
@@ -8,7 +8,69 @@ public class PatternQuestion {
     String Answer2;
     String Answer3;
     String Answer4;
+    String QuestionVi;
+    String Answer1Vi;
+    String Answer2Vi;
+    String Answer3Vi;
+    String Answer4Vi;
     int AnsCorrect;
+
+    public PatternQuestion(int id, int patternId, String question, String answer1, String answer2, String answer3, String answer4, String questionVi, String answer1Vi, String answer2Vi, String answer3Vi, String answer4Vi, int ansCorrect, int isCorrect) {
+        Id = id;
+        PatternId = patternId;
+        Question = question;
+        Answer1 = answer1;
+        Answer2 = answer2;
+        Answer3 = answer3;
+        Answer4 = answer4;
+        QuestionVi = questionVi;
+        Answer1Vi = answer1Vi;
+        Answer2Vi = answer2Vi;
+        Answer3Vi = answer3Vi;
+        Answer4Vi = answer4Vi;
+        AnsCorrect = ansCorrect;
+        this.isCorrect = isCorrect;
+    }
+
+    public String getQuestionVi() {
+        return QuestionVi;
+    }
+
+    public void setQuestionVi(String questionVi) {
+        QuestionVi = questionVi;
+    }
+
+    public String getAnswer1Vi() {
+        return Answer1Vi;
+    }
+
+    public void setAnswer1Vi(String answer1Vi) {
+        Answer1Vi = answer1Vi;
+    }
+
+    public String getAnswer2Vi() {
+        return Answer2Vi;
+    }
+
+    public void setAnswer2Vi(String answer2Vi) {
+        Answer2Vi = answer2Vi;
+    }
+
+    public String getAnswer3Vi() {
+        return Answer3Vi;
+    }
+
+    public void setAnswer3Vi(String answer3Vi) {
+        Answer3Vi = answer3Vi;
+    }
+
+    public String getAnswer4Vi() {
+        return Answer4Vi;
+    }
+
+    public void setAnswer4Vi(String answer4Vi) {
+        Answer4Vi = answer4Vi;
+    }
 
     public int getIsCorrect() {
         return isCorrect;
